2ec5184adbe682a7d81e35eed91fa12c5a306b7f
chibic-yoshida <jun.yoshida@chibic.biz>
2026-03-12 00:20:53 +0900

    Initial Commit

---
+++ qqq/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  qqq
+//
+//  Created by jun on 2026/03/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ qqq/qqqApp.swift
@@ -0,0 +1,17 @@
+//
+//  qqqApp.swift
+//  qqq
+//
+//  Created by jun on 2026/03/12.
+//
+
+import SwiftUI
+
+@main
+struct qqqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
